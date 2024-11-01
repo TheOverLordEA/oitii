4e248141cb9635d4493a9086e835d3135e589ee5
Erkhembayar Atarbold <74484951+TheOverLordEA@users.noreply.github.com>
2024-11-02 03:27:34 +0800

    Added footer style

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -10,14 +10,14 @@ export default function Footer() {
   return (
     <footer className="bg-white rounded-lg shadow dark:bg-gray-900">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-        <div className="sm:flex sm:items-center sm:justify-between">
+        <div className="flex justify-between items-center">
           <Link
             className={`ml-2 tracking-wider text-2xl md:text-4xl font-bold text-black ${josefin_sans.className} `}
             href="/"
           >
             Oitii
           </Link>
-          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+          <ul className="flex flex-wrap items-center text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
             <li>
               <a
                 href="https://www.linkedin.com/company/oitii"
