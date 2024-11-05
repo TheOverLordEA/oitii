718bbb3265a53d812c63e05a1e5840897237b6a1
Erkhembayar Atarbold <74484951+TheOverLordEA@users.noreply.github.com>
2024-11-05 12:15:28 +0800

    Added robot.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,19 +5,31 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: "https://oitii.com",
       lastModified: new Date(),
-      changeFrequency: "yearly",
+      changeFrequency: "monthly",
       priority: 1,
     },
     {
-      url: "https://oitii.com/coming-soon",
+      url: "https://oitii.com/about",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9,
+    },
+    {
+      url: "https://oitii.com/services",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9,
+    },
+    {
+      url: "https://oitii.com/contact",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
-      url: "https://oitii/privacy-policy",
+      url: "https://oitii.com/privacy-policy",
       lastModified: new Date(),
-      changeFrequency: "weekly",
+      changeFrequency: "yearly",
       priority: 0.5,
     },
   ];
